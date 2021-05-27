d1492145003905beb793fffe1e3c91aff186e9ac
Eugene Berezin <35341885+Eugeneberezin@users.noreply.github.com>
2021-05-26 18:45:42 -0700

    TableView + Networking.

--- Combine+UIKIT/Combine+UIKIT/ViewController.swift
+++ Combine+UIKIT/Combine+UIKIT/ViewController.swift
@@ -4,16 +4,66 @@
 //
 //  Created by Eugene Berezin on 5/26/21.
 //
-
+import Combine
 import UIKit
 
 class ViewController: UIViewController {
+    var anyCancelable = Set<AnyCancellable>()
+    var job = [Job]()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        fetchJobs()
+    }
+    
+    private func fetchJobs() {
+        NetworkManager.shared.getResults(description: "", location: "")
+            .receive(on: DispatchQueue.main)
+            .map{$0}
+            .sink { completion in
+    
+                switch completion {
+                
+                case .finished:
+                    print("Done")
+                case .failure(let error):
+                    print(error)
+                }
+            } receiveValue: {[weak self] jobs in
+                guard let self = self else {return}
+                self.job = jobs
+                self.tableView.reloadData()
+                
+            }
+            .store(in: &anyCancelable)
+
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return job.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = job[indexPath.row].company
+        return cell
+    }
+    
+    
 }
 
